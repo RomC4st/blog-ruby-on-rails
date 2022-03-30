c0a61e12fef5f361ef4684b7c285846e98670939
Romain <castillonromain01@gmail.com>
2022-03-30 10:47:12 +0200

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
- resources :article  do
-  post 'create' , on: :collection
-  get 'show', on: :member
-  patch 'update', on: :member
-  delete 'destroy', on: :member
- end
+ resources :article 
 end
