4c4c5bb1a8c3769ea34767f2e88c23579bab26e7
Romain <castillonromain01@gmail.com>
2022-04-15 12:21:08 +0200

    download local-time, add vendor file to gitignore, move translation for local-time in /i18n folder

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,4 @@ pin "application", preload: true
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "local-time", to: "https://ga.jspm.io/npm:local-time@2.1.0/app/assets/javascripts/local-time.js"
+pin "local-time" # @2.1.0
